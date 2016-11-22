4aa40b903af0478389c73efd5f06218a478906c4
Brett <brizow@gmail.com>
2016-11-22 18:47:19 -0500

    Add project files.

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item
+    {
+        public int AmountToHeal { get; set; }
+
+        //Constructor
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal)  :  base(id, name, namePlural) //takes the parameters from the HealingPotion class and passes them to the constructor in the Item.cs
+        { 
+            AmountToHeal = amountToHeal; //send the unique parameter from this class
+        }
+    }
+}
